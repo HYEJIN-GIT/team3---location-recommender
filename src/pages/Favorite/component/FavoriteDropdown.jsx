a1c7c8a040c7d82f04201c7f42fc3e199fb541b4
HYEJIN KWON <chahaa0189@gmail.com>
2026-05-03 13:11:02 +0900

    favorite dropdown 모바일 버전 추가

--- src/pages/Favorite/component/FavoriteDropdown.jsx
+++ src/pages/Favorite/component/FavoriteDropdown.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
 
-const FavoriteDropdown = ({dropdownList,setDropdownList}) => {
+const FavoriteDropdown = ({ dropdownList, setDropdownList }) => {
   return (
     <div className='m-4'>
-      <div className="dropdown dropdown-start">
-  <div tabIndex={0} role="button" className="btn m-1">{dropdownList} ▿</div>
-  <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-    <li><a onClick={()=>{setDropdownList("거리순")}}>가까운순</a></li>
-    <li><a onClick={()=>{setDropdownList("별점순")}}> 별점높은순</a></li>
-  </ul>
-</div>
+
+      {/* 모바일 */}
+      <div className="block md:hidden">
+        <button
+          className="btn w-full"
+          onClick={() => {
+           
+            setDropdownList(prev =>
+              prev === "가까운순" ? "별점순" : "가까운순"
+            )
+          }}
+        >
+          {dropdownList} 변경
+        </button>
+      </div>
+
+      {/* pc*/}
+      <div className="hidden md:block">
+        <div className="dropdown dropdown-start">
+          <div tabIndex={0} role="button" className="btn m-1">
+            {dropdownList} ▿
+          </div>
+
+          <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm">
+            <li>
+              <a onClick={() => setDropdownList("가까운순")}>
+                가까운순
+              </a>
+            </li>
+            <li>
+              <a onClick={() => setDropdownList("별점순")}>
+                별점높은순
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
     </div>
   )
 }
